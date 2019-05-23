dd10b40135b27e5ff598129b3ac2f3c1cef78a72
Dheepashree <50895056+Dheepashree@users.noreply.github.com>
2019-05-23 16:07:37 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,12 +1,11 @@
-N=int(input())
-K=int(input())
-a=[]
+y=input()
+e=input()
+x=y.split()
+N=int(x[0])
+K=int(x[1])
+p=e.split()
 i=1
 c=0
-for i in range(N):
-  b=int(input())
-  a.append(b)
-i=0
 for i in range(K):
-  c=c+a[i]
+  c=c+p[i]
 print(c)

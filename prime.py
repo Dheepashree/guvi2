740f2973a9139cf45415046dbbea75804e48322d
Dheepashree <50895056+Dheepashree@users.noreply.github.com>
2019-05-25 13:02:48 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,11 +1,18 @@
 a=int(input())
 i=2
+count=0
 while(i<a):
   if(a==2):
-    print("yes")
+    break
   elif(a%i==0):
-    print("no")
+    count=count+1
+    break
   else:
-    print("yes")
-  break
-  i=i+1
+    i=i+1
+    continue
+if(count==1):
+  print("no")
+else:
+  print("yes")
+  
+  

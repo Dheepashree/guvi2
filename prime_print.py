91d00ad19bc43c62955ad67e1d075dcb771931b1
Dheepashree <50895056+Dheepashree@users.noreply.github.com>
2019-05-29 12:55:51 +0630

    Update and rename prime.py to prime_print.py

---
+++ prime_print.py
@@ -0,0 +1,22 @@
+a=input().split()
+x=int(a[0])
+y=int(a[1])
+for z in range(x+1,y):
+  i=2
+  count=0
+  if(z==2):
+    print(z)
+  else:
+    while(i<z):
+      if(z%i==0):
+        count=count+1
+        break
+      else:
+        i=i+1
+        continue
+      if(count==1):
+        continue
+      else:
+        print(z)
+  
+  

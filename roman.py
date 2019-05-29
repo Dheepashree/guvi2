941ac0eff29d039c098af2b2053040bb9c9ad352
Dheepashree <50895056+Dheepashree@users.noreply.github.com>
2019-05-29 19:01:16 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,17 @@
+a-input()
+count=0
+for i in a:
+  if(i=='I'):
+    count=count+1
+  elif(i=='V'):
+    if(count>=1 and count<5):
+      count=5-count
+    else:
+      count=count+5
+  elif(i=='X'):
+    if(count==1):
+      count=10-count
+    else:
+      count=count+10
+print(count)
+      

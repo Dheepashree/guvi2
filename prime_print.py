64efced29d456cebef95b32fcb5e07693a1e8c71
Dheepashree <50895056+Dheepashree@users.noreply.github.com>
2019-05-29 13:01:00 +0630

    Update prime_print.py

--- prime_print.py
+++ prime_print.py
@@ -5,7 +5,7 @@ for z in range(x+1,y):
   i=2
   count=0
   if(z==2):
-    print(z)
+    print(z,end=" ")
   else:
     while(i<z):
       if(z%i==0):
@@ -17,6 +17,6 @@ for z in range(x+1,y):
       if(count==1):
         continue
       else:
-        print(z)
+        print(z,end=" ")
   
   
